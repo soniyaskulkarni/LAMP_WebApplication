b37a3c991125bc94aa7d5320342081ed4fd0a4f6
Soniya Shripad Kulkarni <45898535+soniyaskulkarni@users.noreply.github.com>
2022-12-08 14:54:39 +0530

    oss_proj

---
+++ www/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>LAMP STACK</title>
+        <link rel="stylesheet" href="/assets/css/bulma.min.css">
+    </head>
+    <body>
+        <section class="hero is-medium is-info is-bold">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 class="title">
+                        LAMP STACK
+                    </h1>
+                    <h2 class="subtitle">
+                        Your local development environment
+                    </h2>
+                </div>
+            </div>
+        </section>
+        <section class="section">
+            <div class="container">
+                <div class="columns">
+                    <div class="column">
+                        <h3 class="title is-3 has-text-centered">Environment</h3>
+                        <hr>
+                        <div class="content">
+                            <ul>
+                                <li><?= apache_get_version(); ?></li>
+                                <li>PHP <?= phpversion(); ?></li>
+                                <li>
+                                    <?php
+                                    echo "<h3>Hello to PHPMyadmin</h3><h2>ABOUT ME</h2>
+  <p><em>I love to Explore</em></p><p>We have created a fictional personal website/blog, and our fictional character is a hobby photographer.Designing digital experiences and having fun in the technical exploration as well as photography.
+</p>";
+                                    ?>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <h3 class="title is-3 has-text-centered">Quick Links</h3>
+                        <hr>
+                        <div class="content">
+                            <ul>
+                                <li><a href="/phpinfo.php">phpinfo()</a></li>
+                                <li><a href="http://localhost:<? print $_ENV['PMA_PORT']; ?>">phpMyAdmin</a></li>
+                                <li><a href="/test_db.php">Test DB Connection with mysqli</a></li>
+                                <li><a href="/test_db_pdo.php">Test DB Connection with PDO</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
